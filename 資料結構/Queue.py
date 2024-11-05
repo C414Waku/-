6538182d4dc475262d0566db1630a44620c45819
C414Waku <122877177+C414Waku@users.noreply.github.com>
2024-11-05 20:57:56 +0800

    Add files via upload

---
+++ 資料結構/Queue.py
@@ -0,0 +1,41 @@
+class Queue():
+    def __init__(self,x):
+        self.size=x
+        self.data=[0]*self.size
+        self.front=-1
+        self.back=-1
+
+    def isEmpty(self):
+        return self.front==self.back
+    
+    def isFull(self):
+        return self.back==self.size-1
+    
+    def enQueue(self,x):
+        if self.isFull():
+            print("佇列已滿")
+        else:
+            self.back+=1
+            self.data[self.back]=x
+    
+    def deQueue(self):
+        if self.isEmpty():
+            print("佇列已空")
+        else:
+            self.front+=1
+    
+    def printQueue(self):
+        for i  in range(self.front+1,self.back+1):
+            print(self.data[i],end=" ")
+        print() 
+
+def test():
+    q=Queue(5)
+    for i in range(6):
+        q.enQueue(i+1)
+        q.printQueue()
+    for i in range(6):
+        q.deQueue()
+        q.printQueue()
+
+test()
